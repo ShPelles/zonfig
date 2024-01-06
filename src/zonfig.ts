e6544777fd855cde79d1c8e6588f4c1db9b28dcd
Shalom <shalom.pelles@gmail.com>
2024-01-07 01:55:28 +0200

    Support nested schema

--- src/zonfig.ts
+++ src/zonfig.ts
@@ -4,22 +4,41 @@ import EnvReader from "./readers/env-reader";
 const reader = new EnvReader();
 
 export function zonfig<T extends z.ZodObject<z.ZodRawShape>>(schema: T) {
-  const preprocessedSchema = z.preprocess((data) => {
-    if (!isObject(data)) {
-      throw new Error("data must be an object");
+  const preprocessedSchema = z.preprocess((config) => {
+    if (!isObject(config)) {
+      throw new Error("config must be an object");
     }
 
-    for (const key of Object.keys(schema.shape)) {
-      const value = reader.read([key]);
-      const hasValue = value !== undefined;
-      if (hasValue) {
-        data[key] = value;
-      }
-    }
-
-    return data;
+    fillConfig<T>(schema, config);
+    return config;
   }, schema);
   return preprocessedSchema;
+}
+
+function fillConfig<T extends z.ZodObject<z.ZodRawShape>>(
+  schema: T,
+  config: Record<string, unknown>,
+  path: string[] = [],
+) {
+  for (const key of Object.keys(schema.shape)) {
+    if (schema.shape[key] instanceof z.ZodObject) {
+      if (!isObject(config[key])) {
+        config[key] = {};
+      }
+      fillConfig(
+        schema.shape[key] as T,
+        config[key] as Record<string, unknown>,
+        [...path, key],
+      );
+      continue;
+    }
+
+    const value = reader.read([...path, key]);
+    const hasValue = value !== undefined;
+    if (hasValue) {
+      config[key] = value;
+    }
+  }
 }
 
 function isObject(data: unknown): data is Record<string, unknown> {
--- test/zonfig.test.ts
+++ test/zonfig.test.ts
@@ -14,22 +14,35 @@ describe("zonfig()", () => {
 
   test("should fill in given values", () => {
     // Arrange
-    const schema = z.object({ foo: z.string() });
-    const parser = zonfig(schema);
+    const schema = z.object({
+      foo: z.string(),
+      bar: z.object({
+        baz: z.number(),
+      }),
+    });
+
     // Act
-    const result = parser.parse({ foo: "bar" });
+    const parser = zonfig(schema);
+    const result = parser.parse({ foo: "bar", bar: { baz: 123 } });
     // Assert
-    expect(result).toEqual({ foo: "bar" });
+    expect(result).toEqual({ foo: "bar", bar: { baz: 123 } });
   });
 
   test("should fill in values from env", () => {
     // Arrange
-    process.env.TEST001 = "bar";
-    const schema = z.object({ TEST001: z.string() });
-    const parser = zonfig(schema);
+    process.env.ZONFIG1 = "bar";
+    process.env.ZONFIG2_ZONFIG3 = "123";
+    const schema = z.object({
+      zonfig1: z.string(),
+      zonfig2: z.object({
+        zonfig3: z.coerce.number(),
+      }),
+    });
+
     // Act
+    const parser = zonfig(schema);
     const result = parser.parse({});
     // Assert
-    expect(result).toEqual({ TEST001: "bar" });
+    expect(result).toEqual({ zonfig1: "bar", zonfig2: { zonfig3: 123 } });
   });
 });
